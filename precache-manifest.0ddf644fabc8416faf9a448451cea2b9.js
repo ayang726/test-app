2e030afc3127ebbc0f34343871287aedaef34a8f
AwesomePresent <yoursmile128@hotmail.com>
2019-08-10 17:48:38 -0700

    Updates

---
+++ precache-manifest.0ddf644fabc8416faf9a448451cea2b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f94cf43637b0573939aafeaffe259d3",
+    "url": "/test-app/index.html"
+  },
+  {
+    "revision": "fbe7e46f2f9ce31bb0e1",
+    "url": "/test-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "40bce3c5017d55ea373c",
+    "url": "/test-app/static/js/2.1e8d4313.chunk.js"
+  },
+  {
+    "revision": "fbe7e46f2f9ce31bb0e1",
+    "url": "/test-app/static/js/main.23cc819b.chunk.js"
+  },
+  {
+    "revision": "22a76a3ec9a32c9f7abc",
+    "url": "/test-app/static/js/runtime~main.3e334fdd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
